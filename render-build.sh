0eb1b6cd32b815a16bd9f68e20cd0c3db5ab2d2a
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-07 13:35:09 +0800

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# Update package list
-apt-get update
-
-# Install PHP and necessary extensions
-apt-get install -y php php-cli php-mysqli php-common php-json php-opcache php-readline
