a5ec800728b41f6871e055d83fa83c9409327d30
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-08 10:01:46 +0800

    Update csr_logout.php

--- CSR/csr_logout.php
+++ CSR/csr_logout.php
@@ -1,6 +1,27 @@
 <?php
 session_start();
+
+// Unset all session variables
+$_SESSION = [];
+
+// Delete the session cookie (if any)
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+// Destroy the session completely
 session_destroy();
+
+// Redirect to login page
 header("Location: csr_login.php");
 exit;
 ?>
