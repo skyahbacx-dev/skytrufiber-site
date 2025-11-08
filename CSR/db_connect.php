f3f9ee06889f76f7513c8151901d6184ba9fc211
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-08 09:51:26 +0800

    Delete CSR/db_connect.php

--- CSR/db_connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "skytrufiber_db";
-$conn = new mysqli($host, $user, $pass, $db);
-if ($conn->connect_error) { die("DB connection failed: " . $conn->connect_error); }
-?>
