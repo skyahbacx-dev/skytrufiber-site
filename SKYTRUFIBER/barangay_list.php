e193d3d3c280e650d7859d43587b64509d419dd6
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-08 11:27:49 +0800

    Create barangay_list.php

---
+++ SKYTRUFIBER/barangay_list.php
@@ -0,0 +1,162 @@
+<?php
+// Barangay List for Quezon City (Grouped by District)
+?>
+  <optgroup label="District 1">
+    <option>Alicia (Bago Bantay)</option>
+    <option>Bagong Pag-asa (North EDSA / Triangle Park)</option>
+    <option>Bahay Toro (Project 8)</option>
+    <option>Balingasa (Balintawak / Cloverleaf)</option>
+    <option>Bungad (Project 7)</option>
+    <option>Damar</option>
+    <option>Damayan (San Francisco del Monte / Frisco)</option>
+    <option>Del Monte (San Francisco del Monte / Frisco)</option>
+    <option>Katipunan (Muñoz)</option>
+    <option>Lourdes (Sta. Mesa Heights)</option>
+    <option>Maharlika (Sta. Mesa Heights)</option>
+    <option>Manresa</option>
+    <option>Mariblo (SFDM / Frisco)</option>
+    <option>Masambong</option>
+    <option>N.S. Amoranto (Gintong Silahis, La Loma)</option>
+    <option>Nayong Kanluran</option>
+    <option>Paang Bundok (La Loma)</option>
+    <option>Pag-ibig sa Nayon (Balintawak)</option>
+    <option>Paltok (SFDM / Frisco)</option>
+    <option>Paraiso (SFDM / Frisco)</option>
+    <option>Phil-Am (West Triangle)</option>
+    <option>Project 6 (Diliman / Triangle Park)</option>
+    <option>Ramon Magsaysay (Bago Bantay)</option>
+    <option>Saint Peter (Sta. Mesa Heights)</option>
+    <option>Salvacion (La Loma)</option>
+    <option>San Antonio (SFDM / Frisco)</option>
+    <option>San Isidro Labrador (La Loma)</option>
+    <option>San Jose (La Loma)</option>
+    <option>Santa Cruz (Pantranco / Heroes Hill)</option>
+    <option>Santa Teresita (Sta. Mesa Heights)</option>
+    <option>Santo Domingo (Matalahib)</option>
+    <option>Siena</option>
+    <option>Sto. Cristo (Bago Bantay)</option>
+    <option>Talayan</option>
+    <option>Vasra (Diliman)</option>
+    <option>Veterans Village (Project 7 / Muñoz)</option>
+    <option>West Triangle</option>
+  </optgroup>
+
+  <optgroup label="District 2">
+    <option>Bagong Silangan</option>
+    <option>Batasan Hills (Constitution Hills)</option>
+    <option>Commonwealth (Manggahan)</option>
+    <option>Holy Spirit (Don Antonio)</option>
+    <option>Payatas</option>
+  </optgroup>
+
+  <optgroup label="District 3">
+    <option>Camp Aguinaldo</option>
+    <option>Pansol (Balara)</option>
+    <option>Mangga (Cubao)</option>
+    <option>San Roque (Cubao)</option>
+    <option>Silangan (Cubao)</option>
+    <option>Socorro (Araneta City)</option>
+    <option>Bagumbayan (Eastwood)</option>
+    <option>Libis (Eastwood)</option>
+    <option>Ugong Norte (Green Meadows / Corinthian / Ortigas)</option>
+    <option>Masagana (Jacobo Zobel)</option>
+    <option>Loyola Heights (Katipunan)</option>
+    <option>Matandang Balara (Old Balara)</option>
+    <option>East Kamias (Project 1)</option>
+    <option>Quirino 2-A (Project 2 / Anonas)</option>
+    <option>Quirino 2-B (Project 2 / Anonas)</option>
+    <option>Quirino 2-C (Project 2 / Anonas)</option>
+    <option>Amihan (Project 3)</option>
+    <option>Claro (Quirino 3-B)</option>
+    <option>Duyan-duyan (Project 3)</option>
+    <option>Quirino 3-A (Project 3 / Anonas)</option>
+    <option>Bagumbuhay (Project 4)</option>
+    <option>Bayanihan (Project 4)</option>
+    <option>Blue Ridge A (Project 4)</option>
+    <option>Blue Ridge B (Project 4)</option>
+    <option>Dioquino Zobel (Project 4)</option>
+    <option>Escopa I</option>
+    <option>Escopa II</option>
+    <option>Escopa III</option>
+    <option>Escopa IV</option>
+    <option>Marilag (Project 4)</option>
+    <option>Milagrosa (Project 4)</option>
+    <option>Tagumpay (Project 4)</option>
+    <option>Villa Maria Clara (Project 4)</option>
+    <option>E. Rodriguez (Project 5 / Cubao)</option>
+    <option>West Kamias (Project 5 / Kamias)</option>
+    <option>St. Ignatius</option>
+    <option>White Plains</option>
+  </optgroup>
+
+  <optgroup label="District 4">
+    <option>Bagong Lipunan ng Crame (Camp Crame)</option>
+    <option>Botocan (Diliman)</option>
+    <option>Central (Diliman)</option>
+    <option>Damayang Lagi (New Manila)</option>
+    <option>Don Manuel (Galas)</option>
+    <option>Doña Aurora (Galas)</option>
+    <option>Doña Imelda (Sta. Mesa / Galas)</option>
+    <option>Doña Josefa (Galas)</option>
+    <option>Horseshoe</option>
+    <option>Immaculate Concepcion (Cubao)</option>
+    <option>Kalusugan (St. Luke’s)</option>
+    <option>Kamuning</option>
+    <option>Kaunlaran (Cubao)</option>
+    <option>Kristong Hari</option>
+    <option>Krus na Ligas (Diliman)</option>
+    <option>Laging Handa (Diliman)</option>
+    <option>Malaya (Diliman)</option>
+    <option>Mariana (New Manila)</option>
+    <option>Obrero (Project 1)</option>
+    <option>Old Capitol Site (Diliman)</option>
+    <option>Paligsahan (Diliman)</option>
+    <option>Pinagkaisahan (Cubao)</option>
+    <option>Pinyahan (Triangle Park)</option>
+    <option>Roxas (Project 1)</option>
+    <option>Sacred Heart (Kamuning)</option>
+    <option>San Isidro Galas (Galas)</option>
+    <option>San Martin de Porres (Cubao)</option>
+    <option>San Vicente (Diliman)</option>
+    <option>Santol</option>
+    <option>Sikatuna Village (Diliman)</option>
+    <option>South Triangle (Diliman)</option>
+    <option>Sto. Niño (Galas)</option>
+    <option>Tatalon</option>
+    <option>Teacher's Village East (Diliman)</option>
+    <option>Teacher's Village West (Diliman)</option>
+    <option>U.P. Campus (Diliman)</option>
+    <option>U.P. Village (Diliman)</option>
+    <option>Valencia (Gilmore / N. Domingo)</option>
+  </optgroup>
+
+  <optgroup label="District 5">
+    <option>Bagbag (Novaliches)</option>
+    <option>Capri (Novaliches)</option>
+    <option>Fairview (La Mesa / Novaliches)</option>
+    <option>Greater Lagro (Lagro / Novaliches)</option>
+    <option>Gulod (Novaliches)</option>
+    <option>Kaligayahan (Zabarte)</option>
+    <option>Nagkaisang Nayon (General Luis)</option>
+    <option>North Fairview</option>
+    <option>Novaliches Proper (Bayan)</option>
+    <option>Pasong Putik Proper</option>
+    <option>San Agustin (Susano)</option>
+    <option>San Bartolome (Holy Cross)</option>
+    <option>Sta. Lucia (San Gabriel)</option>
+    <option>Sta. Monica</option>
+  </optgroup>
+
+  <optgroup label="District 6">
+    <option>Apolonio Samson (Balintawak)</option>
+    <option>Baesa (Project 8)</option>
+    <option>Balong Bato (Balintawak)</option>
+    <option>Culiat (Tandang Sora)</option>
+    <option>New Era (Central / INC)</option>
+    <option>Pasong Tamo (Tandang Sora)</option>
+    <option>Sangandaan (Project 8)</option>
+    <option>Sauyo (Novaliches)</option>
+    <option>Talipapa (Novaliches)</option>
+    <option>Tandang Sora (Banlat)</option>
+    <option>Unang Sigaw (Balintawak)</option>
+  </optgroup>
