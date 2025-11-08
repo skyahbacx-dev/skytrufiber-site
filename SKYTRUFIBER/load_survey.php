a741e15778e840e161d140e5b56d0ded3c59697d
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-08 09:35:05 +0800

    Update load_survey.php

--- SKYTRUFIBER/load_survey.php
+++ SKYTRUFIBER/load_survey.php
@@ -1,16 +1,34 @@
 <?php
 include '../db_connect.php';
-$res = $conn->query("SELECT * FROM survey ORDER BY created_at DESC");
-echo "<table><tr><th>ID</th><th>Technician</th><th>Client</th><th>Rating</th><th>Remarks</th><th>Date</th></tr>";
-while ($r = $res->fetch_assoc()) {
-  echo "<tr>
-    <td>{$r['id']}</td>
-    <td>".htmlspecialchars($r['tech_name'])."</td>
-    <td>".htmlspecialchars($r['client_name'])."</td>
-    <td>{$r['rating']}</td>
-    <td>".htmlspecialchars($r['remarks'])."</td>
-    <td>{$r['created_at']}</td>
-  </tr>";
+
+try {
+    $stmt = $conn->query("SELECT * FROM survey ORDER BY created_at DESC");
+    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    echo "<table border='1' cellspacing='0' cellpadding='6' style='border-collapse: collapse; width:100%;'>";
+    echo "<tr style='background-color:#009900;color:white;'>
+            <th>ID</th>
+            <th>Technician</th>
+            <th>Client</th>
+            <th>Rating</th>
+            <th>Remarks</th>
+            <th>Date</th>
+          </tr>";
+
+    foreach ($rows as $r) {
+        echo "<tr>
+                <td>" . htmlspecialchars($r['id']) . "</td>
+                <td>" . htmlspecialchars($r['tech_name']) . "</td>
+                <td>" . htmlspecialchars($r['client_name']) . "</td>
+                <td>" . htmlspecialchars($r['rating']) . "</td>
+                <td>" . htmlspecialchars($r['remarks']) . "</td>
+                <td>" . htmlspecialchars($r['created_at']) . "</td>
+              </tr>";
+    }
+
+    echo "</table>";
+
+} catch (PDOException $e) {
+    echo "<p style='color:red;'>❌ Database error: " . htmlspecialchars($e->getMessage()) . "</p>";
 }
-echo "</table>";
 ?>
