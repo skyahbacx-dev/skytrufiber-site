d596baeda33ab6020a6139c979f74b7687042cca
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-16 12:21:11 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -7,8 +7,8 @@ $data = $stmt->fetchAll(PDO::FETCH_ASSOC);
 foreach ($data as $row) {
     echo "
     <div class='client-item' onclick='selectClient({$row['id']}, \"{$row['name']}\")'>
-        <img src='upload/default-avatar.png' class='client-avatar'>
-        <div>
+        <img src=\"lion.png\" class='client-avatar'>
+        <div class='client-info'>
             <b>{$row['name']}</b>
             <p>Assigned to {$row['assigned_csr']}</p>
         </div>
