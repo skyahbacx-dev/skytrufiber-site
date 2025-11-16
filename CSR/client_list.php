fb8f0b08e2b988c57095e337df900be7bd12c787
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-16 11:31:12 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -1,26 +1,24 @@
 <?php
-include '../db_connect.php';
+include "../db_connect.php";
+session_start();
 
-$clients = $conn->query("SELECT id,name,email,assigned_csr,last_active FROM clients ORDER BY name ASC");
+$csr = $_SESSION["csr_user"];
 
-while ($c = $clients->fetch(PDO::FETCH_ASSOC)) {
+$query = $conn->query("
+    SELECT id, name, assigned_csr, last_active
+    FROM clients
+    ORDER BY last_active DESC
+");
 
-    $status = (strtotime($c["last_active"]) > time() - 60) ? "Online" : "Offline";
-    $avatar = ($c["name"][0] <= "M") ? "lion.PNG" : "penguin.PNG";
-
-    echo "
-    <div class='client-item' onclick=\"openClient({$c['id']},'{$c['name']}','{$avatar}')\">
-        <div class='client-main'>
-            <img class='client-avatar' src='{$avatar}'>
-            <div class='client-meta'>
-                <div class='client-name'>{$c['name']}</div>
-                <div class='client-sub'>
-                    <span class='".($status=='Online'?'online-dot':'offline-dot')."'></span>
-                    {$status} • ".($c['assigned_csr'] ?: 'Unassigned')."
-                </div>
-            </div>
+while ($r = $query->fetch(PDO::FETCH_ASSOC)) {
+    echo '
+    <div class="client-item" onclick="openChat('.$r['id'].', \''.$r['name'].'\')">
+        <img src="lion.png" class="client-avatar">
+        <div class="client-info">
+            <b>'.$r["name"].'</b>
+            <p>Assigned to '.$r["assigned_csr"].'</p>
         </div>
     </div>
-    ";
+    ';
 }
 ?>
