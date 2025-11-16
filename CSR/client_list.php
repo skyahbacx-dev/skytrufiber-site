187d4641bb9132c945ef9d57b36ef5e860a93ea1
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-16 11:10:05 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -1,37 +1,22 @@
 <?php
 include "../db_connect.php";
 
-$csr_user = $_SESSION["csr_user"] ?? "";
-
-$stmt = $conn->prepare("
-    SELECT id, name, assigned_csr, last_active
-    FROM clients
-    ORDER BY name ASC
-");
-$stmt->execute();
-
+$stmt = $conn->query("SELECT id, name, assigned_csr, last_active FROM clients ORDER BY name ASC");
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
 
-    $status = "Offline";
-    if ($row["last_active"] && strtotime($row["last_active"]) > time() - 60) {
-        $status = "Online";
-    }
+    $status = (strtotime($row["last_active"]) > time() - 60) ? "Online" : "Offline";
 
-    $avatar = (strtoupper(substr($row["name"], 0, 1)) <= "M")
-                ? "CSR/lion.PNG" : "CSR/penguin.PNG";
-
-    echo "
-    <div class='client-item' onclick=\"openClient({$row['id']}, '{$row['name']}')\">
-        <div class='client-main'>
-            <img src='$avatar' class='client-avatar'>
-            <div class='client-meta'>
-                <div class='client-name'>{$row['name']}</div>
-                <div class='client-sub'>
-                    <span class='".($status=="Online" ? "online-dot" : "offline-dot")."'></span>
-                    $status • ".($row["assigned_csr"] ?: "Unassigned")."
-                </div>
+    echo '
+    <div class="client-item" onclick="openClient('.$row["id"].', \''.$row["name"].'\')">
+        <img src="lion.PNG" class="client-avatar">
+        <div>
+            <div class="client-name">'.$row["name"].'</div>
+            <div class="client-sub">
+                <span class="'.($status=="Online"?"online-dot":"offline-dot").'"></span> '.$status.'
+                • CSR: '.($row["assigned_csr"] ?: "Unassigned").'
             </div>
         </div>
-    </div>";
+    </div>
+    ';
 }
 ?>
