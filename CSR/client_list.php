d2b4931de69a11e73938b766b7184711a3342581
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-16 13:02:15 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -1,18 +1,14 @@
 <?php
 include "../db_connect.php";
-
-$stmt = $conn->query("SELECT id,name,assigned_csr,last_active FROM clients ORDER BY last_active DESC");
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-foreach ($data as $row) {
+$rows = $conn->query("SELECT id,name,assigned_csr,last_active FROM clients ORDER BY last_active DESC")->fetchAll();
+foreach ($rows as $r){
     echo "
-    <div class='client-item' onclick='selectClient({$row['id']}, \"{$row['name']}\")'>
-        <img src=\"lion.png\" class='client-avatar'>
-        <div class='client-info'>
-            <b>{$row['name']}</b>
-            <p>Assigned to {$row['assigned_csr']}</p>
-        </div>
-    </div>
-    ";
+    <div class='client-item' onclick='selectClient({$r['id']}, \"{$r['name']}\")'>
+      <img src='lion.png' class='client-avatar'>
+      <div>
+        <b>{$r['name']}</b>
+        <p style='font-size:12px;color:gray;'>Assigned: {$r['assigned_csr']}</p>
+      </div>
+    </div>";
 }
 ?>
