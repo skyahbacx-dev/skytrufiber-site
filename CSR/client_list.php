310dd56b64be890da8e0349f5732455bfe300cb7
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-18 10:46:57 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -1,22 +1,38 @@
 <?php
 include "../db_connect.php";
+session_start();
 
-$stmt = $conn->query("SELECT id,name,assigned_csr,last_active FROM clients ORDER BY last_active DESC");
-$data = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$csr = $_SESSION["csr_fullname"] ?? $_SESSION["csr_user"];
 
-foreach ($data as $c) {
-    $status = (strtotime($c["last_active"]) > time()-60) ? "online" : "offline";
+$stmt = $conn->query("
+    SELECT id, name, assigned_csr, last_active
+    FROM clients
+    ORDER BY last_active DESC NULLS LAST
+");
+
+while ($c = $stmt->fetch(PDO::FETCH_ASSOC)) {
+
+    // Determine status icon
+    $statusIcon = "<span class='status-dot offline'></span>";
+
+    // Determine control button
+    if ($c["assigned_csr"] === NULL) {
+        // UNASSIGNED
+        $control = "<button class='assign-btn' onclick=\"assignClient({$c['id']})\">➕</button>";
+    }
+    elseif ($c["assigned_csr"] === $csr) {
+        // ASSIGNED TO ME
+        $control = "<button class='remove-btn' onclick=\"removeClient({$c['id']})\">➖</button>";
+    }
+    else {
+        // ASSIGNED TO ANOTHER CSR
+        $control = "<button class='lock-btn' disabled>🔒</button>";
+    }
 
     echo "
-    <div class='client-item' onclick='selectClient({$c["id"]}, \"{$c["name"]}\")'>
-        <img src='lion.png' class='client-avatar'>
-        <div>
-            <div class='client-name'>{$c["name"]}</div>
-            <div class='client-sub'>
-                <span class='{$status}-dot'></span> $status • CSR: {$c["assigned_csr"]}
-            </div>
-        </div>
-    </div>
-    ";
+    <div class='client-item' onclick=\"selectClient({$c['id']}, '{$c['name']}')\">
+        <div class='c-name'>{$c['name']}</div>
+        <div class='c-controls'>$control</div>
+    </div>";
 }
 ?>
