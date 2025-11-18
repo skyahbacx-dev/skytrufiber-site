a13decf0bf61c722d9773328b698d3d800897058
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-18 15:05:03 +0800

    Update csr_chat.js

--- CSR/csr_chat.js
+++ CSR/csr_chat.js
@@ -1,36 +1,30 @@
-/**************** GLOBALS ****************/
 let selectedClient = 0;
 let assignedTo = "";
 let filesToSend = [];
-let galleryItems = [];   // store media paths for swipe gallery
-let galleryIndex = 0;    // current opened index
 
-/**************** SIDEBAR ****************/
+/******** SIDEBAR ********/
 function toggleSidebar(){
     document.querySelector(".sidebar").classList.toggle("open");
     document.querySelector(".sidebar-overlay").classList.toggle("show");
 }
 
-/**************** INFO PANEL ****************/
+/******** SLIDING CLIENT INFO PANEL ********/
 function toggleClientInfo(){
     document.getElementById("clientInfoPanel").classList.toggle("show");
 }
 
-/**************** LOAD CLIENT LIST ****************/
-function loadClients() {
-    $.get("client_list.php", data => {
+/******** LOAD CLIENT LIST ********/
+function loadClients(search="") {
+    $.get("client_list.php?search=" + search, data => {
         $("#clientList").html(data);
-    });
-
-    $(".search").on("keyup", function(){
-        let txt = $(this).val();
-        $.get("client_list.php?search=" + txt, data => {
-            $("#clientList").html(data);
-        });
     });
 }
 
-/**************** SELECT CLIENT ****************/
+$(".search").on("keyup", function(){
+    loadClients($(this).val());
+});
+
+/******** SELECT CLIENT ********/
 function selectClient(id, name, assigned) {
     selectedClient = id;
     assignedTo = assigned;
@@ -40,37 +34,30 @@ function selectClient(id, name, assigned) {
 
     $("#chatName").text(name);
 
-    let locked = assigned && assigned !== csrFullname;
+    let locked = (assigned && assigned !== csrFullname);
+
     $("#messageInput").prop("disabled", locked);
     $("#sendBtn").prop("disabled", locked);
-    if (locked) {
-        $(".upload-icon").hide();
-        $("#fileInput").prop("disabled", true);
-    } else {
-        $(".upload-icon").show();
-        $("#fileInput").prop("disabled", false);
-    }
+    $("#fileInput").prop("disabled", locked);
+
+    if (locked) $(".upload-icon").hide();
+    else $(".upload-icon").show();
 
     loadClientInfo();
     loadMessages();
 }
 
-/**************** ASSIGN & UNASSIGN ****************/
+/******** ASSIGN / UNASSIGN ********/
 function assignClient(id){
-    $.post("assign_client.php", { client_id:id }, function(){
-        loadClients();
-    });
+    $.post("assign_client.php", { client_id:id }, () => loadClients());
 }
-
 function unassignClient(id){
-    if(!confirm("Remove this client from your list?")) return;
-    $.post("unassign_client.php", { client_id:id }, function(){
-        loadClients();
-    });
+    if(!confirm("Remove this client from your assignment?")) return;
+    $.post("unassign_client.php", { client_id:id }, () => loadClients());
 }
 
-/**************** LOAD CLIENT INFO PANEL ****************/
-function loadClientInfo(){
+/******** LOAD CLIENT INFO ********/
+function loadClientInfo() {
     $.getJSON("client_info.php?id=" + selectedClient, info => {
         $("#infoName").text(info.name);
         $("#infoEmail").text(info.email);
@@ -79,35 +66,27 @@ function loadClientInfo(){
     });
 }
 
-/**************** LOAD MESSAGES (CHAT) ****************/
+/******** LOAD MESSAGES ********/
 function loadMessages(){
     if (!selectedClient) return;
 
     $.getJSON("load_chat_csr.php?client_id=" + selectedClient, messages => {
         let html = "";
-        galleryItems = [];  // reset gallery
 
-        messages.forEach((m, index) => {
+        messages.forEach(m => {
             let side = (m.sender_type === "csr") ? "csr" : "client";
 
             html += `<div class="msg ${side}">
                         <div class="bubble">${m.message || ""}`;
 
             if (m.media_path) {
-                galleryItems.push(m.media_path);   // store for viewer
-
                 if (m.media_type === "image") {
-                    html += `
-                        <img src="${m.media_path}" 
-                             class="file-img" 
-                             onclick="openMedia(${galleryItems.length - 1})">
-                    `;
+                    html += `<img src="${m.media_path}" class="file-img" onclick="openMedia('${m.media_path}')">`;
                 } else {
                     html += `
-                        <video class="file-img" controls onclick="openMedia(${galleryItems.length - 1})">
+                        <video controls class="file-img" onclick="openMedia('${m.media_path}')">
                             <source src="${m.media_path}">
-                        </video>
-                    `;
+                        </video>`;
                 }
             }
 
@@ -120,7 +99,7 @@ function loadMessages(){
     });
 }
 
-/**************** FILE PREVIEW ****************/
+/******** PREVIEW MULTIPLE FILES ********/
 $("#fileInput").on("change", function(e){
     filesToSend = [...e.target.files];
     $("#previewArea").html("");
@@ -132,8 +111,7 @@ $("#fileInput").on("change", function(e){
                 <div class="preview-thumb">
                     ${file.type.includes("video")
                         ? `<video src="${ev.target.result}" muted></video>`
-                        : `<img src="${ev.target.result}">`
-                    }
+                        : `<img src="${ev.target.result}">`}
                 </div>
             `);
         };
@@ -141,7 +119,7 @@ $("#fileInput").on("change", function(e){
     });
 });
 
-/**************** SEND MESSAGE ****************/
+/******** SEND MESSAGE ********/
 $("#sendBtn").click(function(){
     let msg = $("#messageInput").val();
     if (!msg && filesToSend.length === 0) return;
@@ -165,53 +143,20 @@ $("#sendBtn").click(function(){
             $("#fileInput").val("");
             filesToSend = [];
             loadMessages();
+            loadClients();
         }
     });
 });
 
-/**************** FULLSCREEN MEDIA VIEWER + SWIPE ****************/
-function openMedia(i){
-    galleryIndex = i;
-    showMedia();
+/******** FULLSCREEN MEDIA MODAL ********/
+function openMedia(src){
     $("#mediaModal").addClass("show");
+    $("#mediaModalContent").attr("src", src);
 }
-
-function showMedia(){
-    $("#mediaModalContent").attr("src", galleryItems[galleryIndex]);
-}
-
 $("#closeMediaModal").click(() => $("#mediaModal").removeClass("show"));
 
-$(".swipe-left").click(() => {
-    galleryIndex = (galleryIndex - 1 + galleryItems.length) % galleryItems.length;
-    showMedia();
-});
-
-$(".swipe-right").click(() => {
-    galleryIndex = (galleryIndex + 1) % galleryItems.length;
-    showMedia();
-});
-
-/* Swipe gesture support */
-let startX = null;
-$("#mediaModal").on("touchstart", e => startX = e.touches[0].clientX);
-$("#mediaModal").on("touchend", e => {
-    let endX = e.changedTouches[0].clientX;
-    if (startX - endX > 80) $(".swipe-right").click(); // slide left
-    if (endX - startX > 80) $(".swipe-left").click();  // slide right
-});
-
-/* Keyboard navigation */
-$(document).on("keydown", e => {
-    if (!$("#mediaModal").hasClass("show")) return;
-
-    if (e.key === "ArrowRight") $(".swipe-right").click();
-    if (e.key === "ArrowLeft")  $(".swipe-left").click();
-    if (e.key === "Escape")     $("#mediaModal").removeClass("show");
-});
-
-/**************** AUTO REFRESH ****************/
-setInterval(loadClients, 4000);
-setInterval(loadMessages, 1500);
+/******** AUTO REFRESH ********/
+setInterval(loadClients, 5000);
+setInterval(loadMessages, 1800);
 
 loadClients();
