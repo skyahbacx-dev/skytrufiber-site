b9a6813391d1fa2a52564709ee13bbff3d419468
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-18 17:20:27 +0800

    Update and rename b2_config.php to b2_upload.php

---
+++ CSR/b2_upload.php
@@ -0,0 +1,42 @@
+<?php
+// b2_upload.php
+// Uploads a file to your Backblaze B2 bucket and returns the public URL
+
+function b2_upload($fileTmp, $fileName) {
+
+    $bucketName  = "ahba-chat-media";
+    $endpoint    = "https://s3.us-east-005.backblazeb2.com";
+    $keyId       = "005a548887f9c4f0000000002";
+    $appKey      = "K005fOYaprINPto/Qdm9wex0w4v/L2k";
+
+    $url = "$endpoint/$bucketName/$fileName";
+
+    $fp = fopen($fileTmp, "r");
+    
+    $headers = [
+        "Authorization: AWS $keyId:$appKey",
+        "x-amz-acl: public-read",
+        "Content-Type: application/octet-stream"
+    ];
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_PUT, true);
+    curl_setopt($ch, CURLOPT_INFILE, $fp);
+    curl_setopt($ch, CURLOPT_INFILESIZE, filesize($fileTmp));
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    $error    = curl_error($ch);
+    $status   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+    curl_close($ch);
+
+    if ($status >= 200 && $status < 300) {
+        return $url; // Public URL success
+    } else {
+        return null;
+    }
+}
+?>
