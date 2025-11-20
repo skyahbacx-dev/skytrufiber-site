46025403de12aa12b7d6ce8bba4761d6ad894ff9
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-20 16:27:19 +0800

    Update client_list.php

--- CSR/client_list.php
+++ CSR/client_list.php
@@ -36,37 +36,38 @@ while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $assigned = $row["assigned_csr"];
     $isMine   = ($assigned === $csrUser);
 
-    // Static default avatar
-    $avatar = "upload/default-avatar.png";
+    $avatar   = "upload/default-avatar.png";
 
-    echo "<div class='client-item' id='client-$id' onclick='selectClient($id, \"$name\", \"$assigned\")'>
-            <div class='client-main'>
-                <img src=\"$avatar\" class='client-avatar'>
-                <div>
-                    <div class='client-name'>$name</div>
-                    <div class='client-sub'>";
+    echo "
+    <div class='client-item' id='client-$id' onclick='selectClient($id, \"$name\", \"$assigned\")'>
+        <img src='$avatar' class='client-avatar'>
+        <div class='client-content'>
+            <div class='client-name'>$name</div>
+            <div class='client-sub'>";
+            
+            if ($assigned === null || $assigned === "") {
+                echo "Unassigned";
+            } elseif ($isMine) {
+                echo "Assigned to YOU";
+            } else {
+                echo "Assigned to $assigned";
+            }
 
-    if ($assigned === null || $assigned === "") {
-        echo "Unassigned";
-    } elseif ($isMine) {
-        echo "Assigned to YOU";
-    } else {
-        echo "Assigned to $assigned";
-    }
+    echo "
+            </div>
+        </div>
+        <div class='client-action'>";
+        
+        if ($assigned === null || $assigned === "") {
+            echo "<button class='pill green' onclick='event.stopPropagation(); assignClient($id)'>➕</button>";
+        } elseif ($isMine) {
+            echo "<button class='pill red' onclick='event.stopPropagation(); unassignClient($id)'>➖</button>";
+        } else {
+            echo "<button class='pill gray' disabled>🔒</button>";
+        }
 
-    echo "</div></div></div>
-            <div class='client-actions'>";
-
-    if ($assigned === null || $assigned === "") {
-        echo "<button class='pill green' onclick='event.stopPropagation(); assignClient($id)'>➕</button>";
-    }
-    elseif ($isMine) {
-        echo "<button class='pill red' onclick='event.stopPropagation(); unassignClient($id)'>➖</button>";
-    }
-    else {
-        echo "<button class='pill gray' disabled title='Handled by $assigned'>🔒</button>";
-    }
-
-    echo "</div></div>";
+    echo "
+        </div>
+    </div>";
 }
 ?>
