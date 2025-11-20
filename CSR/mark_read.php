2705e129ab1861dff0a4bcaf75bbdd3d0c7fe2a7
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-20 16:53:33 +0800

    Update mark_read.php

--- CSR/mark_read.php
+++ CSR/mark_read.php
@@ -1,10 +1,15 @@
 <?php
+session_start();
 include "../db_connect.php";
 
+$csrUser = $_SESSION["csr_user"] ?? "";
 $client = $_POST["client_id"] ?? null;
 
-if ($client) {
-    $stmt = $conn->prepare("UPDATE messages SET is_read=1 WHERE client_id=? AND sender_type='client'");
-    $stmt->execute([$client]);
+if ($client && $csrUser) {
+    $stmt = $conn->prepare("
+        INSERT INTO chat_read (client_id, csr, last_read)
+        VALUES (?, ?, NOW())
+    ");
+    $stmt->execute([$client, $csrUser]);
 }
 ?>
