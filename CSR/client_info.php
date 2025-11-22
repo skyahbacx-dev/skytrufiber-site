c07ba82bf4d5d15fbf21d8d3415afacc235fa550
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-22 16:14:01 +0800

    Update client_info.php

--- CSR/client_info.php
+++ CSR/client_info.php
@@ -1,11 +1,10 @@
 <?php
 session_start();
 include "../db_connect.php";
-header("Content-Type: application/json");
 
 $client_id = $_GET["id"] ?? 0;
 if (!$client_id) {
-    echo json_encode(["error" => "missing id"]);
+    echo json_encode([]);
     exit;
 }
 
@@ -19,8 +18,8 @@ $stmt->execute([":id" => $client_id]);
 $info = $stmt->fetch(PDO::FETCH_ASSOC);
 
 echo json_encode([
-    "name"     => $info["name"] ?? "",
-    "email"    => $info["email"] ?? "",
+    "name"     => $info["name"]     ?? "",
+    "email"    => $info["email"]    ?? "",
     "district" => $info["district"] ?? "",
     "barangay" => $info["barangay"] ?? ""
 ]);
