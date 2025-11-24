058d660e25037e3fe70a19d1e344e82d79d88cce
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-24 12:07:21 +0800

    Update b2_upload.php

--- CSR/b2_upload.php
+++ CSR/b2_upload.php
@@ -1,9 +1,6 @@
 <?php
-// b2_upload.php
-// Uploads a file to your Backblaze B2 bucket and returns the public URL
-
-function b2_upload($fileTmp, $fileName) {
-
+function b2_upload($fileTmp, $fileName)
+{
     $bucketName  = "ahba-chat-media";
     $endpoint    = "https://s3.us-east-005.backblazeb2.com";
     $keyId       = "005a548887f9c4f0000000002";
@@ -12,7 +9,7 @@ function b2_upload($fileTmp, $fileName) {
     $url = "$endpoint/$bucketName/$fileName";
 
     $fp = fopen($fileTmp, "r");
-    
+
     $headers = [
         "Authorization: AWS $keyId:$appKey",
         "x-amz-acl: public-read",
@@ -28,15 +25,8 @@ function b2_upload($fileTmp, $fileName) {
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
     $response = curl_exec($ch);
-    $error    = curl_error($ch);
     $status   = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
     curl_close($ch);
 
-    if ($status >= 200 && $status < 300) {
-        return $url; // Public URL success
-    } else {
-        return null;
-    }
+    return ($status >= 200 && $status < 300) ? $url : null;
 }
-?>
