de866ce58d8aa2c83a9c6a0339c3c1af5e3fdb0d
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-25 15:37:46 +0800

    Update typing_status.php

--- CSR/typing_status.php
+++ CSR/typing_status.php
@@ -1,10 +1,9 @@
 <?php
 include "../db_connect.php";
-header("Content-Type: application/json");
 
-$client_id = (int)($_GET["id"] ?? 0);
+$client_id = intval($_GET["id"] ?? 0);
 
-$stmt = $conn->prepare("SELECT typing FROM typing_status WHERE user_id = :id LIMIT 1");
+$stmt = $conn->prepare("SELECT typing FROM typing_status WHERE client_id = :id LIMIT 1");
 $stmt->execute([":id" => $client_id]);
 
 echo json_encode(["typing" => (bool)$stmt->fetchColumn()]);
