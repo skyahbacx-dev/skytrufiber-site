a02504e3653028d301e782b08fc1ab11c6c77b95
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-25 15:13:35 +0800

    Update and rename SKYTRUFIBER/support_chat/chat_support.php to SKYTRUFIBER/chat_support.php

--- SKYTRUFIBER/chat_support.php
+++ SKYTRUFIBER/chat_support.php
@@ -3,7 +3,7 @@ session_start();
 include "../db_connect.php";
 
 if (!isset($_SESSION["user"])) {
-    header("Location: ../skytrufiber.php");
+    header("Location: skytrufiber.php");
     exit;
 }
 
