f83bdaa7bb366404a58e899c486ac02fab74e354
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-26 16:00:18 +0800

    Update save_chat_csr.php

--- CSR/save_chat_csr.php
+++ CSR/save_chat_csr.php
@@ -1,29 +1,13 @@
 <?php
-include "../db_connect.php";
 session_start();
+include "../db_connect.php";
 
-$csr       = $_SESSION["csr_user"] ?? "";
-$client_id = $_POST["client_id"] ?? 0;
-$message   = $_POST["message"] ?? "";
+$msg = trim($_POST["message"]);
+$id = intval($_POST["client_id"]);
 
-$sql = "INSERT INTO chat (client_id, sender_type, message) VALUES (:c, 'csr', :m) RETURNING id";
-$stmt = $pdo->prepare($sql);
-$stmt->execute([":c" => $client_id, ":m" => $message]);
+$conn->prepare("
+INSERT INTO chat (client_id, sender_type, message, created_at, seen)
+VALUES (:id,'csr',:msg,NOW(),false)
+")->execute([":id"=>$id,":msg"=>$msg]);
 
-$chatId = $stmt->fetchColumn();
-
-if (!empty($_FILES["media"]["name"][0])) {
-    foreach ($_FILES["media"]["tmp_name"] as $i => $tmp) {
-        $filename = time() . "_" . $_FILES["media"]["name"][$i];
-        $path = "../media/" . $filename;
-        move_uploaded_file($tmp, $path);
-
-        $media_type = str_contains($_FILES["media"]["type"][$i], "image") ? "image" : "video";
-
-        $m = $pdo->prepare("INSERT INTO chat_media (chat_id, media_path, media_type) VALUES (:cid, :p, :t)");
-        $m->execute([":cid" => $chatId, ":p" => $filename, ":t" => $media_type]);
-    }
-}
-
-echo json_encode(["status" => "ok"]);
-?>
+echo "OK";
