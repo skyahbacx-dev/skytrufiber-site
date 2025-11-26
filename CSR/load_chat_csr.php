2835208781d0bf5a510368164ab02d25239f3cf1
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-26 21:30:48 +0800

    Update load_chat_csr.php

--- CSR/load_chat_csr.php
+++ CSR/load_chat_csr.php
@@ -1,29 +1,16 @@
 <?php
-session_start();
-require "../db_connect.php";
-$client_id = $_POST["client_id"];
+include "../db_connect.php";
 
-$q = $conn->prepare("SELECT * FROM chat WHERE client_id = ? ORDER BY created_at ASC");
+$client_id = $_POST["client_id"] ?? 0;
+
+$q = $conn->prepare("SELECT * FROM chat WHERE user_id = ? ORDER BY created_at ASC");
 $q->execute([$client_id]);
+
 $messages = $q->fetchAll(PDO::FETCH_ASSOC);
 
 foreach ($messages as $m) {
-    $align = ($m["sender_type"] == "csr") ? "msg-csr" : "msg-client";
+    $side = ($m["sender_type"] === "csr") ? "me" : "them";
 
-    echo "<div class='msg $align'>";
-    echo nl2br($m["message"]);
-
-    // Load media
-    $media = $conn->prepare("SELECT * FROM chat_media WHERE chat_id=?");
-    $media->execute([$m["id"]]);
-    foreach ($media as $md) {
-        if ($md["media_type"] == "image") {
-            echo "<img src='{$md["media_path"]}' class='chat-img' onclick='openMedia(this.src)'>";
-        } else {
-            echo "<video class='chat-video' controls src='{$md["media_path"]}'></video>";
-        }
-    }
-
-    echo "</div>";
+    echo "<div class='message $side'>" . nl2br(htmlspecialchars($m["message"])) . "</div>";
 }
 ?>
