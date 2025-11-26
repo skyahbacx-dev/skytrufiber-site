2aac8deb3ea0d291a941e232cdeb27bffaf68e44
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-26 21:25:41 +0800

    Update load_chat_csr.php

--- CSR/load_chat_csr.php
+++ CSR/load_chat_csr.php
@@ -1,41 +1,29 @@
 <?php
 session_start();
 require "../db_connect.php";
-if (!isset($_SESSION['csr_user'])) exit("Unauthorized");
-
 $client_id = $_POST["client_id"];
-$file = $_FILES["file"];
 
-$bucketName = "ahba-chat-media";
-$endpoint = "https://s3.us-east-005.backblazeb2.com";
-$keyId = "005a548887f9c4f0000000002";
-$appKey = "K005fOYaprINPto/Qdm9wex0w4v/L2k";
+$q = $conn->prepare("SELECT * FROM chat WHERE client_id = ? ORDER BY created_at ASC");
+$q->execute([$client_id]);
+$messages = $q->fetchAll(PDO::FETCH_ASSOC);
 
-$filename = "CSR_" . time() . "_" . basename($file["name"]);
-$path = $endpoint . "/" . $bucketName . "/" . $filename;
+foreach ($messages as $m) {
+    $align = ($m["sender_type"] == "csr") ? "msg-csr" : "msg-client";
 
-$ch = curl_init();
-curl_setopt_array($ch, [
-    CURLOPT_URL => $path,
-    CURLOPT_PUT => true,
-    CURLOPT_INFILE => fopen($file["tmp_name"], "r"),
-    CURLOPT_INFILESIZE => filesize($file["tmp_name"]),
-    CURLOPT_HTTPHEADER => [
-        "Authorization: Basic " . base64_encode("$keyId:$appKey"),
-        "Content-Type: " . mime_content_type($file["tmp_name"])
-    ]
-]);
+    echo "<div class='msg $align'>";
+    echo nl2br($m["message"]);
 
-curl_exec($ch);
-curl_close($ch);
+    // Load media
+    $media = $conn->prepare("SELECT * FROM chat_media WHERE chat_id=?");
+    $media->execute([$m["id"]]);
+    foreach ($media as $md) {
+        if ($md["media_type"] == "image") {
+            echo "<img src='{$md["media_path"]}' class='chat-img' onclick='openMedia(this.src)'>";
+        } else {
+            echo "<video class='chat-video' controls src='{$md["media_path"]}'></video>";
+        }
+    }
 
-$type = explode("/", mime_content_type($file["tmp_name"]))[0] === "image" ? "image" : "video";
-
-$sql = $conn->prepare("INSERT INTO chat_media (chat_id, media_path, media_type) VALUES (
-    (SELECT id FROM chat WHERE client_id=? ORDER BY id DESC LIMIT 1),
-    ?, ?
-)");
-$sql->execute([$client_id, $path, $type]);
-
-echo "OK";
+    echo "</div>";
+}
 ?>
