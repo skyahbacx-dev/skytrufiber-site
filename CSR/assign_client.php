8dbba3971aa261a31cf828ae9eefb4d8cb9e4283
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-26 13:26:21 +0800

    Update assign_client.php

--- CSR/assign_client.php
+++ CSR/assign_client.php
@@ -1,6 +1,22 @@
 <?php
 session_start();
-include '../db_connect.php';
+include "../db_connect.php";
 
-$conn->prepare("UPDATE users SET assigned_csr=? WHERE id=?")
-    ->execute([$_SESSION["csr_user"], $_POST["client_id"]]);
+if (!isset($_SESSION["csr_user"])) {
+    http_response_code(401);
+    exit("Unauthorized");
+}
+
+$csr = $_SESSION["csr_user"];
+$client_id = intval($_POST["client_id"] ?? 0);
+
+if (!$client_id) exit("Invalid client");
+
+$stmt = $conn->prepare("UPDATE users SET assigned_csr = :csr WHERE id = :id");
+$stmt->execute([
+    ":csr" => $csr,
+    ":id" => $client_id
+]);
+
+echo "assigned";
+?>
