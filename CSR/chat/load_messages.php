ec685be9824cdfdaab7f891ff0c4fe2857f87dbc
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-11-27 15:42:17 +0800

    Update load_messages.php

--- CSR/chat/load_messages.php
+++ CSR/chat/load_messages.php
@@ -9,10 +9,12 @@ if (!$clientID) {
 
 try {
     $stmt = $conn->prepare("
-        SELECT sender_type, message, created_at
-        FROM chat
-        WHERE client_id = :cid
-        ORDER BY created_at ASC
+        SELECT c.id, c.sender_type, c.message, c.created_at,
+               m.media_path, m.media_type
+        FROM chat c
+        LEFT JOIN chat_media m ON c.id = m.chat_id
+        WHERE c.client_id = :cid
+        ORDER BY c.created_at ASC
     ");
     $stmt->execute([":cid" => $clientID]);
     $messages = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -24,16 +26,33 @@ try {
 
     foreach ($messages as $m) {
         $sender = $m["sender_type"];
-        $msg    = htmlspecialchars($m["message"]);
+        $msg    = htmlspecialchars($m["message"] ?? "");
         $time   = date("M d g:i A", strtotime($m["created_at"]));
+        $media  = $m["media_path"];
+        $mtype  = $m["media_type"];
 
         $class = ($sender === "client") ? "msg-client" : "msg-csr";
 
-        echo "
-        <div class='chat-bubble $class'>
-            <p>$msg</p>
-            <span class='chat-time'>$time</span>
-        </div>";
+        echo "<div class='chat-bubble $class'>";
+
+        // TEXT
+        if (!empty($msg)) {
+            echo "<p>$msg</p>";
+        }
+
+        // MEDIA PREVIEW
+        if (!empty($media)) {
+            if ($mtype === "image") {
+                echo "<img src='../../$media' class='chat-img' onclick='window.open(\"../../$media\")'>";
+            } elseif ($mtype === "video") {
+                echo "<video class='chat-video' controls><source src='../../$media'></video>";
+            } else {
+                echo "<a href='../../$media' download class='chat-file'>📎 Download File</a>";
+            }
+        }
+
+        echo "<span class='chat-time'>$time</span>";
+        echo "</div>";
     }
 
 } catch (PDOException $e) {
