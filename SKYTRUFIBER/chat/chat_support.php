3352d4960eb1bebe2a8973f9601743ea6cfa97f9
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-01 10:50:24 +0800

    Update chat_support.php

--- SKYTRUFIBER/chat/chat_support.php
+++ SKYTRUFIBER/chat/chat_support.php
@@ -7,21 +7,18 @@
 <title>SkyTruFiber Support</title>
 
 <link rel="stylesheet" href="chat_support.css">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
-
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script src="chat_support.js"></script>
-
 </head>
 <body>
 
-<div id="support-overlay">
+<div id="chat-overlay">
     <div class="chat-modal">
 
         <!-- HEADER -->
         <div class="chat-header">
             <div class="chat-header-left">
-                <img src="/upload/AHBALOGO.png" class="chat-header-logo">
+                <img src="../SKYTRUFIBER.png" class="chat-header-logo">
                 <div>
                     <h2>SkyTruFiber Support</h2>
                     <span class="status active">Support Team Active</span>
@@ -32,21 +29,19 @@
         <!-- CHAT MESSAGES -->
         <div id="chat-messages" class="chat-messages"></div>
 
-        <!-- INLINE PREVIEW -->
+        <!-- PREVIEW INLINE MEDIA -->
         <div id="preview-inline" class="preview-inline">
             <div id="preview-files" class="preview-files"></div>
         </div>
 
         <!-- INPUT BAR -->
         <div class="chat-input-area">
-            <input type="file" id="chat-upload-media" accept="image/*,video/*,application/pdf" multiple hidden>
+            <input type="file" id="chat-upload-media" multiple hidden>
 
-            <button id="upload-btn" class="upload-btn">
-                <i class="fa fa-paperclip"></i>
-            </button>
+            <button id="upload-btn" class="upload-btn">📎</button>
 
             <div class="chat-input-box">
-                <input type="text" id="message-input" placeholder="Write a message..." autocomplete="off">
+                <input id="message-input" type="text" placeholder="Type a message...">
             </div>
 
             <button id="send-btn" class="chat-send-btn">
@@ -54,13 +49,12 @@
             </button>
         </div>
 
+        <button id="scroll-bottom-btn" class="scroll-bottom-btn">
+            <i class="fa fa-arrow-down"></i>
+        </button>
+
     </div>
 </div>
-
-<!-- SCROLL TO BOTTOM BUTTON -->
-<button id="scroll-bottom-btn" class="scroll-bottom-btn">
-    <i class="fa fa-arrow-down"></i>
-</button>
 
 <!-- LIGHTBOX -->
 <div id="lightbox-overlay">
