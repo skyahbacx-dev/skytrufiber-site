03f4f580be8ef2c00398257650c0f27d91959ce3
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-01 14:18:56 +0800

    Update chat_support.js

--- SKYTRUFIBER/chat/chat_support.js
+++ SKYTRUFIBER/chat/chat_support.js
@@ -74,6 +74,11 @@ $(document).ready(function () {
             $("#lightbox-overlay").fadeOut(200);
         }
     });
+$(document).on("click", "video.media-video", function () {
+    const fullSrc = $(this).attr("data-full");
+    $("#lightbox-image").attr("src", fullSrc);
+    $("#lightbox-overlay").fadeIn(200);
+});
 
     // Scroll button
     const box = $("#chat-messages");
