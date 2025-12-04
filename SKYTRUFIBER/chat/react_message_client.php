c3152c01f039d7e4438d92b84cfccfc949fe4f33
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-04 14:26:36 +0800

    Update react_message_client.php

--- SKYTRUFIBER/chat/react_message_client.php
+++ SKYTRUFIBER/chat/react_message_client.php
@@ -7,21 +7,15 @@ $emoji = $_POST["emoji"] ?? "";
 
 if (!$msgID || !$emoji) exit("bad");
 
+// Always "client" in this panel
 $userType = "client";
 
-// Remove old reaction from same user type (simple & universal)
-$delete = $conn->prepare("
-    DELETE FROM chat_reactions 
-    WHERE chat_id = ? AND user_type = ?
-");
-$delete->execute([$msgID, $userType]);
-
-// Insert new reaction
-$insert = $conn->prepare("
+$stmt = $conn->prepare("
     INSERT INTO chat_reactions (chat_id, emoji, user_type)
     VALUES (?, ?, ?)
+    ON CONFLICT (chat_id, user_type)
+    DO UPDATE SET emoji = EXCLUDED.emoji
 ");
-$insert->execute([$msgID, $emoji, $userType]);
+$stmt->execute([$msgID, $emoji, $userType]);
 
 echo "ok";
-?>
