9958b07dc79e6defe3769e1be398fa95bb3915c5
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-04 16:35:55 +0800

    Delete SKYTRUFIBER/chat/typing_update_client.php

--- SKYTRUFIBER/chat/typing_update_client.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once "../../db_connect.php";
-
-$client_id = $_POST["client_id"] ?? null;
-$typing    = $_POST["typing"] ?? 0;
-
-$stmt = $conn->prepare("UPDATE typing_status SET typing=?, updated_at=NOW() WHERE client_id=?");
-$stmt->execute([$typing, $client_id]);
