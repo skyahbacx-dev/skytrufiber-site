92bda896286882f1248bb11dbb923269770cf65c
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-08 15:27:16 +0800

    Update skytrufiber.php

--- SKYTRUFIBER/skytrufiber.php
+++ SKYTRUFIBER/skytrufiber.php
@@ -5,9 +5,7 @@ include '../db_connect.php';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-require '../PHPMailer/src/Exception.php';
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
+
 
 $message = '';
 $forgotMessage = '';
