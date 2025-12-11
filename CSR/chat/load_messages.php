e9ee51860a911f4a5521c0822e750b44b5354da3
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-11 17:03:35 +0800

    Update load_messages.php

--- CSR/chat/load_messages.php
+++ CSR/chat/load_messages.php
@@ -1,4 +1,8 @@
 <?php
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Expires: 0");
+header("Pragma: no-cache");
+
 if (!isset($_SESSION)) session_start();
 require __DIR__ . "/../../db_connect.php";
 
