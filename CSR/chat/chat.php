1534d3a8a7e22cef51ca0fd0bc3f6b5fe44e7c09
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-15 13:10:41 +0800

    Update chat.php

--- CSR/chat/chat.php
+++ CSR/chat/chat.php
@@ -1,5 +1,7 @@
 <?php
-if (!isset($_SESSION)) session_start();
+ini_set("session.name", "CSRSESSID");
+session_start();
+
 
 /* ============================================
    ✔ AUTH CHECK — Must use encrypted CSR routing
