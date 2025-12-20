9d5d09f3718f24e5513b7b2ac74728484976ae66
skyahbacx-dev <sky.ahbacx@gmail.com>
2025-12-20 17:24:16 +0800

    Create db_connect_mysql.php

---
+++ db_connect_mysql.php
@@ -0,0 +1,10 @@
+<?php
+$pdo = new PDO(
+  "mysql:host=localhost;dbname=skytrufiber_db;charset=utf8mb4",
+  "root",
+  "",
+  [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+  ]
+);
