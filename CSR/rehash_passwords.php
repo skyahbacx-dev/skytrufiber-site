51d6bb8fa6b2b536265ae81ac72a4d36bcecd9dd
skyahbacx-dev <sky.ahbacx@gmail.com>
2026-07-15 09:21:05 +0800

    Delete CSR/rehash_passwords.php

--- CSR/rehash_passwords.php
+++
@@ -1,9 +0,0 @@
-<?php
-/* ============================================================
-   DISABLED FOR SECURITY.
-   This tool previously reset staff passwords to a known value
-   and was reachable by anyone on the internet. It is now closed.
-   Use the Super Admin "User Management" panel to reset passwords.
-============================================================ */
-http_response_code(410);
-exit("This tool has been disabled.");
